833b095374948516efd8fa3c3994c5a8517ffcfc
kjhernandez <hernandezkj97@gmail.com>
2021-08-12 15:02:55 -0400

    Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn=document.querySelectorAll('.faq-toggle');
+
+btn.forEach(toggle=>{
+    toggle.addEventListener('click', ()=>{
+        toggle.parentNode.classList.toggle("active");
+    });
+})
+
+
